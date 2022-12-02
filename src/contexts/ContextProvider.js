0dd3da0dfbf2706f04870d34e04f53d6c48db728
Mike Boyd <mboyd3083@gmail.com>
2022-12-02 15:29:04 -0500

    Update to ContextProvider

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -1,63 +1,60 @@
-import {createContext,useContext,useState} from 'react';
+import { createContext, useContext, useState } from "react";
 
 const StateContext = createContext();
 
 const initialState = {
-    chat: false,
-    cart: false,
-    userProfile : false,
-    notification: false,
-}
+  chat: false,
+  cart: false,
+  userProfile: false,
+  notification: false,
+};
 
-export const ContextProvider = ({children}) => {
+export const ContextProvider = ({ children }) => {
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [isClicked, setIsClicked] = useState(initialState);
 
-    const[activeMenu,setActiveMenu] = useState(true);
-    const[isClicked,setIsClicked] = useState(initialState);
+  const [currentColor, setCurrentColor] = useState("#03C9D7");
+  const [currentMode, setCurrentMode] = useState("Light");
+  const [themeSettings, setThemeSettings] = useState(false);
 
-    const[currentColor,setCurrentColor] = useState('#03C9D7');
-    const[currentMode,setCurrentMode] = useState('Light');
-    const[themeSettings,setThemeSettings] = useState(false);
-    
-    const setMode = (e) =>{
-        console.log(e.target.value)
-        setCurrentMode(e.target.value);
+  const setMode = (e) => {
+    setCurrentMode(e.target.value);
 
-        localStorage.setItem('themeMode',e.target.value);
-    }
-    const setColor = (color) =>{
-        console.log(color)
-        setCurrentColor(color);
-        localStorage.setItem('colorMode',color);
-    }
+    localStorage.setItem("themeMode", e.target.value);
+  };
+  const setColor = (color) => {
+    setCurrentColor(color);
+    localStorage.setItem("colorMode", color);
+  };
 
-    const handleClick = (clicked) => {
-    setIsClicked({...initialState, [clicked]: true})
-    }
+  const handleClick = (clicked) => {
+    setIsClicked({ ...initialState, [clicked]: true });
+  };
 
-    const [screenSize, setScreenSize] = useState();
-    return (
-      <StateContext.Provider
-        value={{
-          activeMenu,
-          setActiveMenu,
-          isClicked,
-          setIsClicked,
-          handleClick,
-          screenSize,
-          setScreenSize,
-          currentColor,
-          currentMode,
-          setCurrentColor,
-          setCurrentMode,
-          setColor,
-          setMode,
-          themeSettings,
-          setThemeSettings,
-        }}
-      >
-        {children}
-      </StateContext.Provider>
-    );
-}
+  const [screenSize, setScreenSize] = useState();
+  return (
+    <StateContext.Provider
+      value={{
+        activeMenu,
+        setActiveMenu,
+        isClicked,
+        setIsClicked,
+        handleClick,
+        screenSize,
+        setScreenSize,
+        currentColor,
+        currentMode,
+        setCurrentColor,
+        setCurrentMode,
+        setColor,
+        setMode,
+        themeSettings,
+        setThemeSettings,
+      }}
+    >
+      {children}
+    </StateContext.Provider>
+  );
+};
 
 export const useStateContext = () => useContext(StateContext);
